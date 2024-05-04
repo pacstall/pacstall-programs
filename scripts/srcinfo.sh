992c7dfe9a54d60600f63e387acc7ff08086f635
ook37 <oren+12345@taumoda.com>
2024-05-04 19:38:22 -0400

    move to scripts dir

--- scripts/srcinfo.sh
+++ scripts/srcinfo.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 function vars.srcinfo() {
-  local CARCH='CARCH_REPLACE' DISTRO='ubuntu:jammy' var ar aars bar ars rar _distros _vars _archs _sums distros parsed_distros \
+  local var ar aars bar ars rar _distros _vars _archs _sums distros parsed_distros \
     vars="source depends makedepends optdepends pacdeps checkdepends provides conflicts breaks replaces" \
     archs="amd64 arm64 armel armhf i386 mips64el ppc64el riscv64 s390x" \
     sums="b2 sha512 sha384 sha256 sha224 sha1 md5"
   allvars=(pkgname gives pkgver pkgrel epoch pkgdesc url priority)
   allars=(arch source depends makedepends checkdepends optdepends pacdeps conflicts breaks replaces provides incompatible compatible backup mask noextract nosubmodules license maintainer repology)
-  mapfile -t parsed_distros < <(awk 'NF && !/:$/ {sub(/\/.*/, "", $1); print $1}' distrolist)
-  distros="${parsed_distros[*]}"
+  distros=$(awk -v ORS=' ' 'NF && !/:$/ {sub(/\/.*/, "", $1); print $1}' distrolist)
   _distros="{${distros// /,}}" _vars="{${vars// /,}}" _archs="{${archs// /,}}" _sums="{${sums// /,}}"
   eval "allars+=(${_vars}_${_distros} ${_vars}_${_archs} ${_vars}_${_distros}_${_archs} ${_sums}sums ${_sums}sums_${_distros} ${_sums}sums_${_archs} ${_sums}sums_${_distros}_${_archs})"
   eval "allvars+=(gives_${_distros} gives_${_archs} gives_${_distros}_${_archs})"
@@ -16,6 +15,7 @@ function vars.srcinfo() {
 }
 
 function gen.srcinfo() {
+  local CARCH='CARCH_REPLACE' DISTRO='ubuntu:jammy'
   # shellcheck disable=SC1090
   source "${1}"
   for var in "${allvars[@]}"; do
@@ -51,8 +51,9 @@ function gen.srcinfo() {
 
 vars.srcinfo
 for i in "${@}"; do
-  gen.srcinfo "${i}" > "${i%/*.pacscript}"/.SRCINFO
+  gen.srcinfo "${i}" > "${i%/*.pacscript}"/.SRCINFO &
 done
+wait
 unset allars allvars
 
 # vim:set ft=sh ts=2 sw=4 noet:
