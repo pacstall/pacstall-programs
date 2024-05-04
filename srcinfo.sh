ba6f8ef17a07b3164303ba057b186ae3569705d8
ook37 <oren+12345@taumoda.com>
2024-05-04 18:39:37 -0400

    improve

--- srcinfo.sh
+++ srcinfo.sh
@@ -1,44 +1,17 @@
 #!/usr/bin/env bash
 
 function srcinfo() {
-  local CARCH='CARCH_REPLACE' DISTRO='ubuntu:jammy' valid_distros=("${2}") vdist src sum vars a_sum var ar car aars bar ars rar \
-    known_hashsums_src=("b2" "sha512" "sha384" "sha256" "sha224" "sha1" "md5") \
-    known_archs_src=("amd64" "arm64" "armel" "armhf" "i386" "mips64el" "ppc64el" "riscv64" "s390x") \
-    allvars=("pkgname" "gives" "pkgver" "pkgrel" "epoch" "pkgdesc" "url" "priority") \
-    allars=("arch" "source" "depends" "makedepends" "checkdepends" "optdepends" "pacdeps" "conflicts" "breaks" \
-    "replaces" "provides" "incompatible" "compatible" "backup" "mask" "noextract" "nosubmodules" "license" "maintainer" "repology")
-  for vars in {source,depends,makedepends,optdepends,pacdeps,checkdepends,provides,conflicts,breaks,replaces}; do
-    for vdist in "${valid_distros[@]}"; do
-      allars+=("${vars}_${vdist}")
-    done
-    for src in "${known_archs_src[@]}"; do
-      allars+=("${vars}_${src}")
-      for s_vdist in "${valid_distros[@]}"; do
-        allars+=("${vars}_${s_vdist}_${src}")
-      done
-    done
-  done
-  for vdist in "${valid_distros[@]}"; do
-    allvars+=("gives_${vdist}")
-  done
-  for src in "${known_archs_src[@]}"; do
-    allvars+=("gives_${src}")
-    for vdist in "${valid_distros[@]}"; do
-      allvars+=("gives_${vdist}_${src}")
-    done
-  done
-  for sum in "${known_hashsums_src[@]}"; do
-    allars+=("${sum}sums")
-    for vdist in "${valid_distros[@]}"; do
-      allars+=("${sum}sums_${vdist}")
-    done
-    for a_sum in "${known_archs_src[@]}"; do
-      allars+=("${sum}sums_${a_sum}")
-      for vdist in "${valid_distros[@]}"; do
-        allars+=("${sum}sums_${vdist}_${a_sum}")
-      done
-    done
-  done
+  local CARCH='CARCH_REPLACE' DISTRO='ubuntu:jammy' var ar aars bar ars rar _distros _vars _archs _sums distros parsed_distros \
+    vars="source depends makedepends optdepends pacdeps checkdepends provides conflicts breaks replaces" \
+    archs="amd64 arm64 armel armhf i386 mips64el ppc64el riscv64 s390x" \
+    sums="b2 sha512 sha384 sha256 sha224 sha1 md5" \
+    allvars=(pkgname gives pkgver pkgrel epoch pkgdesc url priority) \
+    allars=(arch source depends makedepends checkdepends optdepends pacdeps conflicts breaks replaces provides incompatible compatible backup mask noextract nosubmodules license maintainer repology)
+  mapfile -t parsed_distros < <(awk 'NF && !/:$/ {sub(/\/.*/, "", $1); print $1}' distrolist)
+  distros="${parsed_distros[*]}"
+  _distros="{${distros// /,}}" _vars="{${vars// /,}}" _archs="{${archs// /,}}" _sums="{${sums// /,}}"
+  eval "allars+=(${_vars}_${_distros} ${_vars}_${_archs} ${_vars}_${_distros}_${_archs} ${_sums}sums ${_sums}sums_${_distros} ${_sums}sums_${_archs} ${_sums}sums_${_distros}_${_archs})"
+  eval "allvars+=(gives_${_distros} gives_${_archs} gives_${_distros}_${_archs})"
   # shellcheck disable=SC1090
   source "${1}"
   for var in "${allvars[@]}"; do
@@ -70,36 +43,10 @@ function srcinfo() {
   unset "${allars[@]}" "${allvars[@]}"
 }
 
-write_all() {
-  local packagelist
-  mapfile -t packagelist < packagelist
-  export -f srcinfo
-  # shellcheck disable=SC2016
-  for package in "${packagelist[@]}"; do
-      srcinfo packages/"${package}"/"${package}".pacscript "${parsed_distros[@]}" | tee packages/"${package}"/.SRCINFO > /dev/null
-  done
-}
-
-fetch_distros() {
-  mapfile -t parsed_distros < <(curl -fsSL https://salsa.debian.org/debian/distro-info-data/-/raw/main/{ubuntu.csv,debian.csv} \
-    | awk -F ',' -v current_date="$(date +'%Y-%m-%d')" '
-      NR > 1 && (($6 > current_date) || ($7 > current_date) || ($6 == "")) &&
-      ($4 <= current_date) && $3 != "series" && $3 != "experimental" {
-        print $3
-      }
-      END {print "devel"}
-  ')
-  export parsed_distros_str="${parsed_distros[*]}"
-}
-
 (($# <= 0)) && echo "You failed to specify a pacscript." && exit 1
-fetch_distros
 
-case ${1} in
-  "write_all") write_all ;;
-  *.pacscript) srcinfo "${1}" "${parsed_distros[@]}" ;;
-  *) echo "Please specify a pacscript or use write_all." && exit 1 ;;
-esac
-unset parsed_distros parsed_distros_str
+for i in "${@}"; do
+  srcinfo "${i}" > "${i%/*.pacscript}"/.SRCINFO
+done
 
 # vim:set ft=sh ts=2 sw=4 noet:
