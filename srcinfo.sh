60f59007e76de6ac3580fb58bb5d08fc6fbbc643
ook37 <oren+12345@taumoda.com>
2024-05-04 19:22:59 -0400

    dont overloop

--- srcinfo.sh
+++ srcinfo.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-function srcinfo() {
+function vars.srcinfo() {
   local CARCH='CARCH_REPLACE' DISTRO='ubuntu:jammy' var ar aars bar ars rar _distros _vars _archs _sums distros parsed_distros \
     vars="source depends makedepends optdepends pacdeps checkdepends provides conflicts breaks replaces" \
     archs="amd64 arm64 armel armhf i386 mips64el ppc64el riscv64 s390x" \
-    sums="b2 sha512 sha384 sha256 sha224 sha1 md5" \
-    allvars=(pkgname gives pkgver pkgrel epoch pkgdesc url priority) \
-    allars=(arch source depends makedepends checkdepends optdepends pacdeps conflicts breaks replaces provides incompatible compatible backup mask noextract nosubmodules license maintainer repology)
+    sums="b2 sha512 sha384 sha256 sha224 sha1 md5"
+  allvars=(pkgname gives pkgver pkgrel epoch pkgdesc url priority)
+  allars=(arch source depends makedepends checkdepends optdepends pacdeps conflicts breaks replaces provides incompatible compatible backup mask noextract nosubmodules license maintainer repology)
   mapfile -t parsed_distros < <(awk 'NF && !/:$/ {sub(/\/.*/, "", $1); print $1}' distrolist)
   distros="${parsed_distros[*]}"
   _distros="{${distros// /,}}" _vars="{${vars// /,}}" _archs="{${archs// /,}}" _sums="{${sums// /,}}"
   eval "allars+=(${_vars}_${_distros} ${_vars}_${_archs} ${_vars}_${_distros}_${_archs} ${_sums}sums ${_sums}sums_${_distros} ${_sums}sums_${_archs} ${_sums}sums_${_distros}_${_archs})"
   eval "allvars+=(gives_${_distros} gives_${_archs} gives_${_distros}_${_archs})"
+  export allars allvars
+}
+
+function gen.srcinfo() {
   # shellcheck disable=SC1090
   source "${1}"
   for var in "${allvars[@]}"; do
@@ -45,8 +49,10 @@ function srcinfo() {
 
 (($# <= 0)) && echo "You failed to specify a pacscript." && exit 1
 
+vars.srcinfo
 for i in "${@}"; do
-  srcinfo "${i}" > "${i%/*.pacscript}"/.SRCINFO
+  gen.srcinfo "${i}" > "${i%/*.pacscript}"/.SRCINFO
 done
+unset allars allvars
 
 # vim:set ft=sh ts=2 sw=4 noet:
