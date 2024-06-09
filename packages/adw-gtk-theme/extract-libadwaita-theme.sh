d334df052506186576bfa8ce13f9c9c6f26a6bae
ook37 <oren+12345@taumoda.com>
2024-06-09 15:29:09 -0400

    arch users

--- packages/adw-gtk-theme/extract-libadwaita-theme.sh
+++ packages/adw-gtk-theme/extract-libadwaita-theme.sh
@@ -3,10 +3,10 @@ rm -rf /usr/share/themes/Adw{,-dark}/gtk-4.0
 mkdir -p /usr/share/themes/Adw{,-dark}/gtk-4.0
 for rpath in $(gresource list /usr/lib/libadwaita-1.so | grep '^/org/gnome/Adwaita/styles/'); do
   resource=${rpath#/org/gnome/Adwaita/styles/}
-  if [[ $resource == */* ]]; then
+  if [[ ${resource} == */* ]]; then
     mkdir -p /usr/share/themes/Adw/gtk-4.0/"${resource%/*}"
   fi
-  gresource extract /usr/lib/libadwaita-1.so "$rpath" > "/usr/share/themes/Adw/gtk-4.0/$resource"
+  gresource extract /usr/lib/libadwaita-1.so "${rpath}" > "/usr/share/themes/Adw/gtk-4.0/$resource"
   ln -sfrT /usr/share/themes/Adw{,-dark}/gtk-4.0/"${resource%%/*}"
 done
 cat /usr/share/themes/Adw/gtk-4.0/{base,defaults-light}.css > /usr/share/themes/Adw/gtk-4.0/gtk.css
